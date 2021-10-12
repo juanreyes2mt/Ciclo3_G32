7471a70d16c4c5ea074ec8ad336de1a43f3bfe2a
JUAN CARLOS REYES <juan.reyes2.mt@correo.usa.edu.co>
2021-10-11 22:49:05 -0500

    Cargue de primera versión de index.html para el reto 2

---
+++ js/misFunciones.js
@@ -0,0 +1,137 @@
+/* Funciones para manejadores tabla BICICLETA */
+
+function ConsultarBicicletas(){
+    $.ajax({
+        url:"https://gd2d1fb23ab6545-db202109302209.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuestaBike){
+            console.log(respuestaBike);
+            listarBicicletas(respuestaBike.items);
+        }
+
+    });
+}
+
+function listarBicicletas(items){
+
+    let myTable="<table>";
+    for(i=0;i<items.length;i++){
+        myTable+="<tr>";
+        myTable+="<td">+items[i].id+"</td>";
+        myTable+="<td>"+items[i].brand+"</td>";
+        myTable+="<td>"+items[i].model+"</td>";
+        myTable+="<td>"+items[i].category_id+"</td>";
+        myTable+="<td>"+items[i].name+"</td>";
+        myTable+="<td> <button onclick='borrarElemento("+items[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#consultaBike").append(myTable);
+
+}
+
+/* Funciones para manejadores de tabla CLIENTE */
+
+function traerInformacion(){
+    $.ajax({
+        url:"https://g42463fc9ec0dbe-db202109261450.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta.items);
+        }
+
+    });
+}
+
+function pintarRespuesta(items){
+
+    let myTable="<table>";
+    for(i=0;i<items.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+items[i].id+"</td>";
+        myTable+="<td>"+items[i].name+"</td>";
+        myTable+="<td>"+items[i].email+"</td>";
+        myTable+="<td>"+items[i].age+"</td>";
+        myTable+="<td> <button onclick='borrarElemento("+items[i].id+")'>Borrar</button>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#consultaBike").append(myTable);
+
+}
+function guardarInformacion(){
+    let myData={
+        id:$("#id").val(),
+        name:$("#name").val(),
+        email:$("#email").val(),
+        age:$("#age").val(),
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g42463fc9ec0dbe-db202109261450.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"POST",
+        data:myData,
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#name").val("");
+            $("#email").val("");
+            $("#age").val("");
+            traerInformacion();
+            alert("se ha guardado el dato")
+        }
+    });
+}
+
+
+function editarInformacion(){
+    let myData={
+        id:$("#id").val(),
+        name:$("#name").val(),
+        email:$("#email").val(),
+        age:$("#age").val(),
+
+    };
+    console.log(myData);
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g42463fc9ec0dbe-db202109261450.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"PUT",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            $("#id").val("");
+            $("#name").val("");
+            $("#email").val("");
+            $("#age").val("");
+            traerInformacion();
+            alert("se ha Actualizado")
+        }
+    });
+}
+
+function borrarElemento(idElemento){
+    let myData={
+        id:idElemento
+    };
+    let dataToSend=JSON.stringify(myData);
+    $.ajax({
+        url:"https://g42463fc9ec0dbe-db202109261450.adb.sa-saopaulo-1.oraclecloudapps.com/ords/admin/client/client",
+        type:"DELETE",
+        data:dataToSend,
+        contentType:"application/JSON",
+        datatype:"JSON",
+        success:function(respuesta){
+            $("#resultado").empty();
+            traerInformacion();
+            alert("Se ha Eliminado.")
+        }
+    });
+}
+
